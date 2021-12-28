875446f3ef79c5a70175f319223667bc98fa2f7d
harsha <harsha6286@gmail.com>
2021-12-28 21:28:02 +0530

    requirements.txt added

--- AILobby/urls.py
+++ AILobby/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('AIWeb.urls'))
+    path('', include('AIWeb.urls')),
+    path('captcha', include('captcha.urls')),
 ]
--- AIWeb/models.py
+++ AIWeb/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django import forms
+from captcha.fields import CaptchaField
+
+
+class contactForm(forms.Form):
+    captcha = CaptchaField()
+
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=30, default="not mentioned")
+    email = models.EmailField(default="not mentioned")
+    phone = models.CharField(max_length=15, default="not mentioned")
+    desc = models.TextField(default="not mentioned")
+
+    def __str__(self):
+        return f'{self.name} - {self.desc[:15]} ..'
