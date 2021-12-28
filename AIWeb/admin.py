3af447409ef11827ed167e4c51dcc63f3bc1adb5
harsha <harsha6286@gmail.com>
2021-12-29 00:02:11 +0530

    projects completed This is a major step in project all necessary fields in contact object added and clodinary image storage done I think no need to touch anything in project again....

--- AIWeb/admin.py
+++ AIWeb/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact,Project
 # Register your models here.
 admin.site.register(Contact)
+admin.site.register(Project)
