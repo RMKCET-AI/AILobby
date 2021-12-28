26264c085d880b48737b5a0e017c23d596582a9c
harsha <harsha6286@gmail.com>
2021-12-28 21:28:02 +0530

    Contact page working Captcha Field working proper and Captcha model registered in admin.py

--- AIWeb/views.py
+++ AIWeb/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact,contactForm
 
 
 # Create your views here.
@@ -7,7 +8,23 @@ def index(request):
 
 
 def contact(request):
-    return render(request, 'AIWeb/contact.html')
+    if request.method == "POST":
+        form = contactForm(request.POST)
+        if form.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
+            phone = request.POST['phone']
+            concern = request.POST['desc']
+            print(name, email, phone, concern)
+            contactObj = Contact(name=name, email=email, phone=phone, desc=concern)
+            contactObj.save()
+            form = contactForm()
+            return render(request, 'AIWeb/contact.html', {'form': form, 'submitted': True, 'try': True})
+        else:
+            form = contactForm()
+            return render(request, 'AIWeb/contact.html', {'form': form, 'submitted': False, 'try': True})
+    form = contactForm()
+    return render(request, 'AIWeb/contact.html', {'form': form, 'submitted': False, 'Failed': False})
 
 
 def projects(request):
