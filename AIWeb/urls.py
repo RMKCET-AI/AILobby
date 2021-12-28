93a1d257b738f65c617acec1bb02a39ffa884ae3
harsha <harsha6286@gmail.com>
2021-12-28 16:54:55 +0530

    Templates and Static files Added templates and static images and ready for deployment

---
+++ AIWeb/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+admin.site.site_header = "AI Lobby"
+admin.site.site_title = "Welcome to AILobby dashboard"
+admin.site.index_title = "Manage your site here"
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('index', views.index, name="home"),
+    path('contact', views.contact, name="contact"),
+    path('projects', views.projects, name="projects"),
+    path('events', views.events, name="events"),
+    path('academics', views.academics, name="academics")
+]
--- AIWeb/views.py
+++ AIWeb/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'AIWeb/index.html')
+
+
+def contact(request):
+    return render(request, 'AIWeb/contact.html')
+
+
+def projects(request):
+    return render(request, 'AIWeb/projects.html')
+
+
+def events(request):
+    return render(request, 'AIWeb/events.html')
+
+
+def academics(request):
+    return render(request, 'AIWeb/academics.html')
